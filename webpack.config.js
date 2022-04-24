f4df71d97d1f35b6c24c8c3f4686ebca2c9a8490
Remiferiaa <slong819@hotmail.com>
2022-04-24 23:15:28 +0800

    Page title Change

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'To Do List',
+      title: 'Weather App',
     }),
     new MiniCssExtractPlugin({
       filename: "style.css"
